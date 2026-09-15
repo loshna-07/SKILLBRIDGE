deb27c3eb7e4a9eeb935a7bd2581b090553d6224
loshna-07 <loshna180407@gmail.com>
2026-09-15 10:24:11 +0530

    Execute targeted deletion of 5 test student accounts

--- server/src/routes/statsRoutes.ts
+++ server/src/routes/statsRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { getPublicStats, getWeights, updateWeights, inspectStudents } from '../controllers/statsController';
+import { getPublicStats, getWeights, updateWeights, inspectStudents, deleteTargetedTestStudents } from '../controllers/statsController';
 import { authenticateUser, authorizeRoles } from '../middleware/authMiddleware';
 
 const router = Router();
 
 router.get('/public', getPublicStats);
 router.get('/inspect-students', inspectStudents);
+router.post('/delete-target-students', deleteTargetedTestStudents);
 router.get('/weights', getWeights);
 router.put('/weights', authenticateUser, authorizeRoles('INSTITUTION'), updateWeights);
 
